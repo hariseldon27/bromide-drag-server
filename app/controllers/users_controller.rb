67f9eab14290670458b79ab62216ce31ba6728ff
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-04-07 10:03:42 -0700

    users controller checks for avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,13 @@ class UsersController < ApplicationController
         render json: users
     end
     def show
-        if current_user 
-            render json: {message: "you're in", user: current_user, avatar: rails_blob_path(current_user.avatar, disposition: "attachment") }
-        else 
-            render json: { error: "You need to authorize"}
-        end
+        render json: {message: "you're in", user: current_user, avatar: show_avatar}
+        # byebug
+        # render json: {message: "you're in", user: current_user, avatar: rails_blob_path(current_user.avatar, disposition: "attachment") }
+        # user.avatar ? render json: {message: "you're in", user: current_user, avatar: rails_blob_path(current_user.avatar, disposition: "attachment") }
+        
+
+        
     end
     def add_profile_photo
         user = current_user
@@ -23,5 +25,12 @@ class UsersController < ApplicationController
     private
     def user_params
         params.permit(:email, :profile_photo)
+    end
+    def show_avatar
+        if current_user.avatar.attached?
+            return rails_blob_path(current_user.avatar, disposition: "attachment")
+        else 
+            return
+        end
     end
 end
