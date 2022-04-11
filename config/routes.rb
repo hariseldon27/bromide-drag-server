edd0f7fcaf30b86370b0acc67d945920e49d6d73
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-04-11 06:19:29 -0700

    nothing really

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/member-data', to: 'users#show'
   patch '/profile-photo/:id', to: 'users#add_profile_photo'
   
-
+    # POST => gallery#create /new-gallery create new gallery
+    # POST => gallery#block-builder /new-gallery/id/block-builder/
 end
